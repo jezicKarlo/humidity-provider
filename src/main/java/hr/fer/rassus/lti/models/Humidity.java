43e1224e19d84589ed5b7ac1c99d033a26cec463
jezicKarlo <karlo.jezic@gmail.com>
2021-01-05 14:37:33 +0100

    implement HUmidity and repository

--- src/main/java/hr/fer/rassus/lti/models/Humidity.java
+++ src/main/java/hr/fer/rassus/lti/models/Humidity.java
@@ -1,4 +1,25 @@
 package hr.fer.rassus.lti.models;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Humidity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer humidity;
+
+    public Humidity() {
+    }
+
+    public Humidity(Integer humidity) {
+        this.humidity = humidity;
+    }
 }
--- src/main/java/hr/fer/rassus/lti/repository/HumidityRepository.java
+++ src/main/java/hr/fer/rassus/lti/repository/HumidityRepository.java
@@ -4,4 +4,6 @@ import hr.fer.rassus.lti.models.Humidity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HumidityRepository extends JpaRepository<Humidity, Integer> {
+
+    Humidity getById(Integer id);
 }
