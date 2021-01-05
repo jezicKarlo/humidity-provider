15151efc8256fd06c6d52e5f1fb0718a8eb85d55
jezicKarlo <karlo.jezic@gmail.com>
2021-01-05 14:39:13 +0100

    add and implement Response

--- src/main/java/hr/fer/rassus/lti/controller/Entrypoint.java
+++ src/main/java/hr/fer/rassus/lti/controller/Entrypoint.java
@@ -1,8 +1,11 @@
 package hr.fer.rassus.lti.controller;
 
 import hr.fer.rassus.lti.service.HumidityService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.core.Response;
 
 @RestController
 @RequestMapping("/api/humidity")
