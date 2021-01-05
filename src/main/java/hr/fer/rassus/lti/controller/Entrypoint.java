4b67d741bf2c276ac6a9afa8fc7be9c086845c20
jezicKarlo <karlo.jezic@gmail.com>
2021-01-05 14:39:47 +0100

    imports

--- src/main/java/hr/fer/rassus/lti/controller/Entrypoint.java
+++ src/main/java/hr/fer/rassus/lti/controller/Entrypoint.java
@@ -1,11 +1,11 @@
 package hr.fer.rassus.lti.controller;
 
+import hr.fer.rassus.lti.response.Response;
 import hr.fer.rassus.lti.service.HumidityService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.ws.rs.core.Response;
 
 @RestController
 @RequestMapping("/api/humidity")
