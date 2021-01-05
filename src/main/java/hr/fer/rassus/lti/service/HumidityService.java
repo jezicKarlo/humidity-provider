c7a399465aa2d809ddeed1fd36b3928687942aa0
jezicKarlo <karlo.jezic@gmail.com>
2021-01-05 14:35:32 +0100

    implement service and add repositor

--- src/main/java/hr/fer/rassus/lti/service/HumidityService.java
+++ src/main/java/hr/fer/rassus/lti/service/HumidityService.java
@@ -1,4 +1,27 @@
 package hr.fer.rassus.lti.service;
 
+import hr.fer.rassus.lti.repository.HumidityRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HumidityService {
+
+    private final HumidityRepository repository;
+
+    public HumidityService(HumidityRepository repository) {
+        this.repository = repository;
+    }
+
+    public Integer getHumidity() {
+        int id = generateId();
+        return repository.findAll().get(id).getHumidity();
+        //return repository.getById(id).getHumidity();
+    }
+
+    private int generateId() {
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+        int minute = now.getMinute();
+        return 4 * hour + minute / 15;
+    }
 }
